514ebcca2eb5d0e8a019aa7bf330799c2f765226
frank-qcd-qk <cxq41@case.edu>
2018-11-13 11:59:49 -0500

    finished fixing code, formatted the documents

--- Source Code/brute_force.m
+++ Source Code/brute_force.m
@@ -14,49 +14,68 @@
 clear all
 load 'intercity_distances.dat'; %read in the intercity distances
 temp = size(intercity_distances);
-Ncities=temp(1);
-all_solutions=[];
+Ncities = temp(1);
+all_solutions = [];
 %set first and last cities to city 1:
-trip = ones(1,11);
-ntries=0;
-nvalid=0;
-for c2=2:6  %range of values for city2; consider cities 7 through 10 only for c10; generates
-                    %trips that are not redundant forward vs reverse order
-    for c3=2:10
-        for c4=2:10
-            for c5=2:10
-                for c6=2:10
-                    for c7=2:10
-                        for c8=2:10
-                            for c9=2:10
-                                for c10=7:10 %no need to repeat any values used by c2; can consider reverse-order trip
-                                    ntries=ntries+1;
-                                    if (0 == mod(ntries,100000))
+trip = ones(1, 11);
+ntries = 0;
+nvalid = 0;
+
+for c2 = 2:6%range of values for city2; consider cities 7 through 10 only for c10; generates
+    %trips that are not redundant forward vs reverse order
+    for c3 = 2:10
+
+        for c4 = 2:10
+
+            for c5 = 2:10
+
+                for c6 = 2:10
+
+                    for c7 = 2:10
+
+                        for c8 = 2:10
+
+                            for c9 = 2:10
+
+                                for c10 = 7:10%no need to repeat any values used by c2; can consider reverse-order trip
+                                    ntries = ntries + 1;
+
+                                    if (0 == mod(ntries, 100000))
                                         ntries
                                     end
-                                    trip=[1,c2,c3,c4,c5,c6,c7,c8,c9,c10,1]; %need to check if this trip is viable
-                                    ndistinct=num_cities_visited(trip); 
-                                    if ndistinct==Ncities %valid tour
+
+                                    trip = [1, c2, c3, c4, c5, c6, c7, c8, c9, c10, 1]; %need to check if this trip is viable
+                                    ndistinct = num_cities_visited(trip);
+
+                                    if ndistinct == Ncities%valid tour
                                         trip
-                                        dist=eval_soln(trip,intercity_distances)
-                                        all_solutions=[all_solutions,dist];
-                                        nvalid=nvalid+1
-                                         %pause
+                                        dist = eval_soln(trip, intercity_distances)
+                                        all_solutions = [all_solutions, dist];
+                                        nvalid = nvalid + 1
+                                        %pause
                                     end
+
                                 end
+
                             end
+
                         end
+
                     end
+
                 end
+
             end
+
         end
+
     end
+
 end
+
 mu = mean(all_solutions)
 sigma = std(all_solutions)
 best = min(all_solutions)
 worst = max(all_solutions)
-hist(all_solutions,100)
+hist(all_solutions, 100)
 title('cost histogram of viable trips')
-               
-           
--- Source Code/num_cities_visited.m
+++ Source Code/num_cities_visited.m
@@ -2,11 +2,13 @@
 %returns Ncities if the trip has no repeats (except for returning to
 %originating city)
 function [ndistinct] = num_cities_visited(trip)
-temp=size(trip);
-Ncities=temp(2);
-citylist = zeros(1,Ncities); %check these off as they are visited
-for i=1:Ncities
-    city=trip(i);
-    citylist(city)=1;
-end
-ndistinct = sum(citylist);
+    temp = size(trip);
+    Ncities = temp(2);
+    citylist = zeros(1, Ncities); %check these off as they are visited
+
+    for i = 1:Ncities
+        city = trip(i);
+        citylist(city) = 1;
+    end
+
+    ndistinct = sum(citylist);
